a2e89d0f88feaee3a54b53e0bdb65a59190bc206
imelva <elva.hao.yang@gmail.com>
2015-09-23 03:33:07 -0400

    first commit

---
+++ js/myscript.js
@@ -0,0 +1,40 @@
+/*!
+ * Start Bootstrap - Agency Bootstrap Theme (http://startbootstrap.com)
+ * Code licensed under the Apache License v2.0.
+ * For details, see http://www.apache.org/licenses/LICENSE-2.0.
+ */
+
+// jQuery for page scrolling feature - requires jQuery Easing plugin
+$(function() {
+    $('a.page-scroll').bind('click', function(event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: $($anchor.attr('href')).offset().top
+        }, 1500, 'easeInOutExpo');
+        event.preventDefault();
+    });
+});
+
+// Highlight the top nav as scrolling occurs
+$('body').scrollspy({
+    target: '.navbar-fixed-top'
+})
+
+// Closes the Responsive Menu on Menu Item Click
+$('.navbar-collapse ul li a').click(function() {
+    $('.navbar-toggle:visible').click();
+});
+
+// Filtering experiences
+var $btns = $('.btn').click(function() {
+  if (this.id == 'All') {
+    $('#parent > div').fadeIn(450);
+  } else {
+    var $el = $('.' + this.id).fadeIn(450);
+    $('#parent > div').not($el).hide();
+  }
+  $btns.removeClass('active');
+  $btns.removeClass('btn-inverse');
+  $(this).addClass('active');
+  $(this).addClass('btn-inverse');
+})
