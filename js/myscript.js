71a98ca6c42883c850d82bf9ad441ba99ceb4227
imelva <elva.hao.yang@gmail.com>
2015-09-25 02:49:25 -0400

    Added animations and portfolio content

--- js/myscript.js
+++ js/myscript.js
@@ -4,6 +4,72 @@
  * For details, see http://www.apache.org/licenses/LICENSE-2.0.
  */
 
+ var cover_slide = 1;
+
+function cover_transition() {
+  var c = $(".cover-hero-item:nth-child(" + cover_slide + ")");
+  if (cover_slide == 6) {
+      var b = $(".cover-hero-item:nth-child(1)");
+      cover_slide = 1
+  } else {
+      var b = $(".cover-hero-item:nth-child(" + (cover_slide + 1) + ")");
+      cover_slide++
+  }
+  b.removeClass("cover-hero-item-inactive");
+  setTimeout(function() {
+      c.addClass("cover-hero-item-hidden");
+      b.addClass("cover-hero-item-active");
+      setTimeout(function() {
+          c.addClass("cover-hero-item-inactive");
+          setTimeout(function() {
+              c.removeClass("cover-hero-item-hidden cover-hero-item-active")
+          }
+          , 0)
+      }
+      , 200)
+  }
+  , 0);
+}
+$(document).ready(function() {
+    setInterval(function() {
+        cover_transition()
+    }
+    , 1500);
+
+    // Magnific popup
+    magnific_pop_up();
+
+});
+
+function magnific_pop_up() {
+  if( $("a.image-link").length){
+     $("a.image-link").click(function (e) {
+        var items = [];          
+        items.push( { src: $(this).attr('href')  } );
+        if($(this).data('gallery')){
+            var $arraySrc = $(this).data('gallery').split(',');
+            $.each( $arraySrc, function( i, v ){
+                items.push( {
+                    src: v 
+                });
+            });     
+        } 
+        $.magnificPopup.open({
+            type:'image',
+            mainClass: 'mfp-fade',
+            items:items,
+            gallery: {
+              enabled: true 
+            }
+        });
+        e.preventDefault();
+    });
+  }
+  if( $("a.image-iframe").length){
+       $('a.image-iframe').magnificPopup({type:'iframe',mainClass: 'mfp-fade'});
+  }
+}
+
 // jQuery for page scrolling feature - requires jQuery Easing plugin
 $(function() {
     $('a.page-scroll').bind('click', function(event) {
@@ -38,3 +104,4 @@ var $btns = $('.btn').click(function() {
   $(this).addClass('active');
   $(this).addClass('btn-inverse');
 })
+
